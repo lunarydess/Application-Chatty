a1d90dc29e0c4b2f03eec576145bfcc432cfaa52
lunarydess <lucielle.hoerner@outlook.com>
2024-08-17 19:54:40 +0200

    :rocket: deploy project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import java.nio.charset.StandardCharsets
+
+plugins {
+    id("idea")
+    id("java")
+    id("java-library")
+}
+
+group = "zip.luzey"
+version = "0.0.0-develop"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_17.toString()
+    targetCompatibility = JavaVersion.VERSION_17.toString()
+    options.encoding = StandardCharsets.UTF_8.toString()
+}
+
+tasks.withType<AbstractArchiveTask> {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
---
+++ src/main/java/zip/luzey/chatty/Chatty.java
@@ -0,0 +1,5 @@
+package zip.luzey.chatty;
+
+public final class Chatty {
+    public static void main(final String... args) {}
+}
